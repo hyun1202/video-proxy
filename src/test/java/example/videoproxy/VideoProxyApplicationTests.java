a41c876a0ca29eb7f56300e8dfa0821a79ff8999
hyun <apfhd0257@naver.com>
2025-04-08 13:42:05 +0900

    init

---
+++ src/test/java/example/videoproxy/VideoProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package example.videoproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
