baa32c14a6bd75c75f66ac8398942019e5a955e1
hyun <apfhd0257@naver.com>
2025-05-15 14:58:24 +0900

    chore: null일 경우 origin 로그 안찍도록 수정

--- src/main/java/example/videoproxy/filter/VideoCorsFilter.java
+++ src/main/java/example/videoproxy/filter/VideoCorsFilter.java
@@ -21,24 +21,29 @@ import java.util.List;
 public class VideoCorsFilter implements WebFilter {
     private final AllowOrigin allowOrigins;
 
-//    private final List<String> allowOrigins = List.of(
-//            "https://ecuwdemo2698.cafe24.com",
-//            "https://ecudemo357864.cafe24.com"
-//    );
-
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         String origin = exchange.getRequest().getHeaders().getOrigin();
         origin = removeEndSeparation(origin);
+
+        if (origin == null) {
+            return getForbiddenResponse(exchange, origin);
+        }
+
         log.info("origin = {}", origin);
-        if (origin == null || !allowOrigins.contains(origin)) {
-            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-            exchange.getResponse().getHeaders().add("X-Reason", "Invalid or missing Origin: " + (origin != null ? origin : "null"));
-            return exchange.getResponse().setComplete();
+
+        if (!allowOrigins.contains(origin)) {
+            return getForbiddenResponse(exchange, origin);
         }
 
         exchange.getResponse().getHeaders().add("Access-Control-Allow-Origin", origin);
         return chain.filter(exchange);
+    }
+
+    private static Mono<Void> getForbiddenResponse(ServerWebExchange exchange, String origin) {
+        exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+        exchange.getResponse().getHeaders().add("X-Reason", "Invalid or missing Origin: " + (origin != null ? origin : "null"));
+        return exchange.getResponse().setComplete();
     }
 
     private String removeEndSeparation(String origin) {
